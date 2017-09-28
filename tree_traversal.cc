95538a513da577bd9528cf8ad3807f98a50f8648
Keivan <keivan.zavari@gmail.com>
2017-09-28 10:00:17 +0200

    added postOrder

--- tree_traversal.cc
+++ tree_traversal.cc
@@ -33,3 +33,25 @@ void preOrder(node * root) {
         preOrder(root->right);
     
 }
+
+
+
+/**
+* - Check if the current node is empty / null.
+* - Traverse the left subtree by recursively calling the post-order function.
+* - Traverse the right subtree by recursively calling the post-order function.
+* - Display the data part of the root (or current node).
+*/
+void postOrder(node *root) {
+    if (root == NULL)
+    {
+        std::cout << std::endl;
+        return;
+    }
+    
+    if (root->left != NULL) 
+        postOrder(root->left);
+    if (root->right != NULL) 
+        postOrder(root->right);
+    std::cout << root->data << " ";
+}
