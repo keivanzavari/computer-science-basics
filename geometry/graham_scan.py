9ff5ff58673a9f2ab2f5c5b8ffcc602fd3633352
Keivan Zavari <keivan.zavari@gmail.com>
2021-02-26 16:24:25 +0100

    Add graham scan algorithm for convex hull calculation

---
+++ geometry/graham_scan.py
@@ -0,0 +1,185 @@
+#! /usr/bin/env python3
+from typing import List, Tuple
+import matplotlib.pyplot as plt
+import math
+
+# -----------------------------
+# Graham Scan algorithm
+# let points be the list of points
+# let stack = empty_stack()
+#
+# find the lowest y-coordinate and leftmost point, called P0
+# sort points by polar angle with P0, if several points have the same polar angle then only keep the farthest
+#
+# for point in points:
+#     # pop the last point from the stack if we turn clockwise to reach this point
+#     while count stack > 1 and ccw(next_to_top(stack), top(stack), point) <= 0:
+#         pop stack
+#     push point to stack
+# end
+# -----------------------------
+
+
+def are_almost_equal(val1: float, val2: float, absolute_error: float = 1e-6) -> bool:
+    return (abs(val1 - val2) <= abs(absolute_error))
+
+
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, el):
+        self.data.append(el)
+
+    def pop(self):
+        return self.data.pop()
+
+    def top(self):
+        if (self.data):
+            return self.data[-1]
+        raise RuntimeError("stack is empty")
+
+    def next_to_top(self):
+        if (self.data) and len(self.data) >= 2:
+            return self.data[-2]
+        raise RuntimeError("stack is not large enough")
+
+    def __len__(self):
+        return len(self.data)
+
+
+class Point:
+    def __init__(self, x: float = 0.0, y: float = 0.0):
+        self.x = x
+        self.y = y
+
+    def __lt__(self, other: "Point") -> bool:
+        if are_almost_equal(self.y, other.y):
+            return self.x < other.x
+        return self.y < other.y
+
+    def __eq__(self, other: "Point") -> bool:
+        return are_almost_equal(self.x, other.x) and are_almost_equal(self.y, other.y)
+
+    def __str__(self) -> str:
+        return f"({self.x}, {self.y})"
+
+    def __hash__(self) -> str:
+        return hash((self.x, self.y))
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def __add__(self, other: "Point"):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: "Point"):
+        return Point(self.x - other.x, self.y - other.y)
+
+
+def cross(point1: Point, point2: Point) -> Tuple[float, float, float]:
+    # homogeneous representation of a point is (x,y,1)
+    cross_x = point1.y - point2.y
+    cross_y = point2.x - point1.x
+    cross_z = point1.x * point2.y - point1.y * point2.x
+    return cross_x, cross_y, cross_z
+
+
+def get_distance(point1: Point, point2: Point) -> float:
+    diff = point1 - point2
+    return math.sqrt(diff.x * diff.x + diff.y * diff.y)
+
+
+def get_angle(point1: Point, point2: Point) -> float:
+    # the line through two points is L=p cross p'
+    # ax+by+c=0
+    a, b, _ = cross(point1, point2)
+    return math.atan2(-a, b)
+
+
+def test_sorting_based_on_polar_angle(points: List[Point]) -> None:
+    sorted_points = sorted(points[1:], key=lambda p: get_angle(points[0], p))
+
+    idx = 1
+    margin = 0.2
+    for p in sorted_points:
+        plt.plot([points[0].x, p.x], [points[0].y, p.y], ":")
+        plt.text(p.x + margin, p.y + margin, f"point {idx}")
+        idx += 1
+    # The numbers should appear in ascending order with respect to the angle they make with point zero.
+
+
+def compute_ccw(point1: Point, point2: Point, point3: Point):
+    """
+    Determining whether three points constitute a "left turn" or a "right turn" does not require computing
+    the actual angle between the two line segments, and can actually be achieved with simple arithmetic only.
+    For three points P1, P2 and P3, compute the z-coordinate of the cross product of the two vectors P1 P2 and P1P3,
+    which is given by the expression:
+    (P2.x - P1.x) * (P3.y - P1.y) - (P2.y - P1.y) * (P3.x - P1.x)
+    If the result is 0, the points are collinear; if it is positive, the three points constitute a
+    "left turn" or counter-clockwise orientation, otherwise a "right turn" or clockwise orientation
+    (for counter-clockwise numbered points).
+    """
+    _, _, res = cross(point2 - point1, point3 - point1)
+
+    return res
+
+
+class Range:
+    def __init__(self, lo: float, hi: float) -> None:
+        self.lo = lo
+        self.hi = hi
+
+
+def generate_random_points(num_points: int, points_range: Range) -> List[Point]:
+    import random
+    # seed random number generator generate random numbers between 0-1
+    random.seed(1)
+    values = []
+    for _ in range(num_points):
+        values.append(points_range.lo + random.random() * (points_range.hi - points_range.lo))
+
+    points = set()
+    while len(points) < num_points:
+        x = random.choice(values)
+        y = random.choice(values)
+        points.add(Point(x, y))
+    return list(points)
+
+
+def move_left_bottom_to_first(points: List[Point]):
+    min_point = min(points)
+    idx = points.index(min_point)
+    points[0], points[idx] = points[idx], points[0]
+
+
+if __name__ == "__main__":
+    # points = [Point(1, 2.5), Point(-2, -5), Point(-1, 3), Point(2.0, 0.5), Point(1.0, 5.0), Point(0, -1), Point(8, 0)]
+
+    points = generate_random_points(20, Range(-5, 5))
+    move_left_bottom_to_first(points)
+    sorted_points = sorted(points[1:], key=lambda p: get_angle(points[0], p))
+    sorted_points.insert(0, points[0])
+
+    stack = Stack()
+    for point in sorted_points:
+        # pop the last point from the stack if we turn clockwise to reach this point
+        angle = 0.0
+        if len(stack) >= 2:
+            while len(stack) >= 2 and compute_ccw(stack.next_to_top(), stack.top(), point) < 0:
+                p = stack.pop()
+        stack.push(point)
+
+    plt.figure()
+    for p in points:
+        plt.plot(p.x, p.y, "*", markersize=12)
+
+    x = [p.x for p in stack.data]
+    y = [p.y for p in stack.data]
+    x.append(x[0])
+    y.append(y[0])
+    plt.plot(x, y, "-")
+
+    test_sorting_based_on_polar_angle(points)
+    plt.grid()
+    plt.show()
---
+++ sorting/merge_sort.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+// There are many functions here that are already part of STL library, many either in <algorithm> or <utility>.
+// Functions like swap, min, max, copy, etc.
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& container) {
+  os << "[";
+  for (const auto& v : container) {
+    os << v << " ";
+  }
+  os << "]";
+  return os;
+}
+
+// break down the array into smallest elements i.e. vectors of size 1.
+// then merge them back with comparison between the elements.
+// merge({2, 8}, {1, 4}) should return {1,2,4,8}
+template <typename T>
+std::vector<T> merge(const std::vector<T>& vec1, const std::vector<T>& vec2) {
+  std::size_t idx1 = 0;
+  std::size_t idx2 = 0;
+  std::size_t smaller_vec_size = (vec1.size() > vec2.size()) ? vec2.size() : vec1.size();
+  std::size_t idx_res = 0;
+  std::vector<T> res(vec1.size() + vec2.size());
+  while (idx1 < vec1.size() && idx2 < vec2.size()) {
+    if (vec1[idx1] <= vec2[idx2]) {
+      res[idx_res++] = vec1[idx1++];
+    } else {
+      res[idx_res++] = vec2[idx2++];
+    }
+  }
+
+  if (idx2 == vec2.size()) {
+    std::copy(vec1.begin() + idx1, vec1.end(), res.begin() + idx_res);
+  } else {
+    std::copy(vec2.begin() + idx2, vec2.end(), res.begin() + idx_res);
+  }
+  return res;
+}
+
+template <typename T>  //, typename Iterator>
+std::vector<T> sort(const std::vector<T>& vec, std::size_t begin, std::size_t end) {
+  if (vec.empty()) {
+    return {};
+  }
+  if (end - begin <= 1) {
+    return {vec[begin]};
+  }
+
+  std::size_t size = (end - begin) / 2;
+
+  // divide
+  auto vec1 = sort(vec, begin, begin + size);
+  auto vec2 = sort(vec, begin + size, end);
+
+  return merge(vec1, vec2);
+}
+
+int main() {
+  std::vector<int> vec{10, 2, -1};
+  std::cout << "original vector: " << vec << "\n";
+
+  auto sorted = sort(vec, 0, vec.size());
+  std::cout << "sorted vector " << sorted << "\n";
+  return 0;
+}
