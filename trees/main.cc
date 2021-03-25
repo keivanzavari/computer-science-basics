dd1cf0bb440b0c095e965e4e86a3e634e6e7f4d1
Keivan Zavari <keivan.zavari@gmail.com>
2021-03-25 22:57:07 +0100

    Add a main for testing.

---
+++ trees/main.cc
@@ -0,0 +1,20 @@
+#include <fstream>
+#include <iostream>
+
+#include "dot_writer.h"
+#include "tree_definitions.h"
+
+int main() {
+  tree::Node* root = nullptr;
+  root = tree::insert(root, 100);
+  for (int i = 0; i < 10; ++i) {
+    tree::insert(root, i);
+  }
+
+  std::cout << "root: " << root->value << "\n";
+  // std::cout << "child: " << root->right->value << "\n";
+  std::ofstream myfile("example.dot");
+  tree::exportDot(myfile, root);
+
+  return 0;
+}
