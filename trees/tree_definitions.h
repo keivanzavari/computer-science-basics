c5bb5939d0cdd2abe8d756badc1244fbf99f5774
Keivan Zavari <keivan.zavari@gmail.com>
2021-03-27 17:02:24 +0100

    Place if else statement of insert in curly brackets.

--- trees/tree_definitions.h
+++ trees/tree_definitions.h
@@ -19,12 +19,13 @@ struct Node {
 
 template <typename T>
 Node<T>* insert(Node<T>* root, T value) {
-  if (!root)
+  if (!root) {
     root = new Node(value);
-  else if (value < root->value)
+  } else if (value < root->value) {
     root->left = insert(root->left, value);
-  else  // key >= root->key
+  } else {  // key >= root->key
     root->right = insert(root->right, value);
+  }
   return root;
 }
 
