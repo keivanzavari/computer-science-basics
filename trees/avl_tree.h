4fd3ddf187a7382ca43737032750c4ca532187fa
Keivan Zavari <keivan.zavari@gmail.com>
2021-03-27 20:11:50 +0100

    Add method to balance tree after insertion

--- trees/avl_tree.h
+++ trees/avl_tree.h
@@ -13,6 +13,38 @@ class Avl {
   }
 
  private:
+  void balanceTree() {
+    int balance_factor = getBalance(root);
+    if (balance_factor == 0 || balance_factor == 1 || balance_factor == -1) {
+      // these are all good cases we don't need to do anything else.
+      std::cout << "good case, balance didn't break.\n";
+      return;
+    }
+
+    if (balance_factor > 1) {
+      // left case is when balance factor is positive. left is longer than right.
+      auto left = root->left;
+      if (bst::getHeight(left->left) > bst::getHeight(left->right)) {
+        // if the height of the right child is greater than the height of the left child, then we have a right right
+        // case.
+        root = bst::balanceLeftLeft(root);
+      } else {
+        // left, right case.
+        root = bst::balanceLeftRight(root);
+      }
+    } else if (balance_factor < 1) {
+      // right case is when balance factor is negative. right is longer than left.
+      auto right = root->right;
+      if (bst::getHeight(right->right) > bst::getHeight(right->left)) {
+        // if the height of the right child is greater than the height of the left child, then we have a right right
+        // case.
+        root = bst::balanceRightRight(root);
+      } else {
+        // right, left case.
+        root = bst::balanceRightLeft(root);
+      }
+    }
+  }
   int getBalance(Node<T>* node) {
     if (!node) {
       return 0;
