1dbaa51c38ff90898025e82c95246e02ea689cbb
Keivan Zavari <keivan.zavari@gmail.com>
2021-04-11 22:32:05 +0200

    Add a simple implementation for a hash table.

---
+++ hashing/linked_list.py
@@ -0,0 +1,70 @@
+from typing import Any
+
+
+class PairNode:
+    def __init__(self, key: Any, value: Any) -> None:
+        self.k = key
+        self.v = value
+        self.next: PairNode = None
+
+    def __repr__(self):
+        if self.next is None:
+            return f"({self.k}, {self.v}) -> ()"
+        return f"({self.k}, {self.v}) -> {self.next.__repr__()}"
+
+
+class LinkedListIterator:
+    def __init__(self, head: PairNode) -> None:
+        self.current = head
+
+    def __iter__(self) -> "LinkedListIterator":
+        return self
+
+    def __next__(self) -> PairNode:
+        if self.current is None:
+            raise StopIteration
+        else:
+            item = self.current
+            self.current = self.current.next
+            return item
+
+
+class LinkedList:
+    def __init__(self, key: Any, value: Any):
+        self.head = PairNode(key, value)
+
+    def __repr__(self):
+        return f"{self.head.__repr__()}"
+
+    def __iter__(self):
+        return LinkedListIterator(self.head)
+
+    def insert_end(self, key: Any, value: Any) -> None:
+        new_node = PairNode(key, value)
+        end = self.head
+        while end.next is not None:
+            end = end.next
+        end.next = new_node
+
+    def insert_begin(self, key: Any, value: Any) -> None:
+        begin = PairNode(key, value)
+        begin.next = self.head
+        self.head = begin
+
+    def search(self, key: Any) -> Any:
+        node = self.head
+        while True:
+            if node.k == key:
+                return node.v
+            if node is None or node.next is None:
+                raise KeyError(f"Key {key} doesn't exist.")
+
+            node = node.next
+
+
+if __name__ == '__main__':
+    p = LinkedList(1, 2)
+    p.insert_end(3, 4)
+    p.insert_begin(6, 7)
+    for item in p:
+        print(f"({item.k}, {item.v})")
---
+++ hashing/simple_dict.py
@@ -0,0 +1,114 @@
+from typing import Any, Optional
+from linked_list import LinkedList
+# Pre-hash from given keys to integer keys
+
+# hash the integer keys to an index in the hash table of size m
+# why should these two be separate?
+
+
+def init_table(size: int) -> list:
+    t = []
+    for _ in range(size):
+        t.append(None)
+    return t
+
+
+class DictWithChaining:
+    def __init__(self):
+        self.table_size = 2
+        self.num_items = 0
+        self.table = init_table(self.table_size)
+
+    def __repr__(self):
+        ret = "-----------\n"
+        for row in self.table:
+            if row is None:
+                ret += "O\n"
+            else:
+                ret += row.__repr__() + "\n"
+        ret += "-----------\n"
+        return ret
+
+    def get_index(self, k: Any, size: int) -> int:
+        hk = hash(k)
+        index = hk % size
+        return index
+
+    def generic_insert(self, generic_table: list, k: Any, v: Any, size: int) -> bool:
+        idx = self.get_index(k, size)
+        inserted = False
+        if generic_table[idx] is None:
+            generic_table[idx] = LinkedList(k, v)
+            inserted = True
+        else:
+            # Unique keys
+            if generic_table[idx].head.k == k:
+                generic_table[idx].head.v = v
+            else:
+                generic_table[idx].insert_begin(k, v)
+                inserted = True
+        return inserted
+
+    def insert(self, k: Any, v: Any):
+        inserted = self.generic_insert(self.table, k, v, self.table_size)
+        if inserted:
+            self.num_items += 1
+        self.maybe_grow()
+
+    def delete(self, k: Any):
+        idx = self.get_index(k, self.table_size)
+        self.table[idx] = None
+
+    def search(self, k: Any):
+        idx = self.get_index(k, self.table_size)
+        # search should make sure to return the correct value if
+        # the index has more than one element.
+        return self.table[idx].search(k)
+
+    def resize(self, new_size: int) -> None:
+        print(f"resize is called to new size {new_size}")
+        # resize the table, either grow or shrink.
+        # after resizing, we'll need to rehash
+        new_table = init_table(new_size)
+        for item in self.table:
+            if item is not None:
+                for pair in item:
+                    self.generic_insert(new_table, pair.k, pair.v, new_size)
+
+        self.table = new_table
+        self.table_size = new_size
+
+    def maybe_grow(self) -> None:
+        if self.num_items >= self.table_size:
+            new_table_size = self.table_size * 2
+            self.resize(new_table_size)
+
+    def mayb_shrink(self) -> None:
+        if self.table_size >= (self.num_items / 4):
+            new_table_size = int(self.table_size / 2)
+            self.resize(new_table_size)
+
+
+if __name__ == '__main__':
+    # keys = ["a", "b", "c", "1", "2", "3"]
+    # m = 16
+    # for key in keys:
+    #     hk = hash(key)
+    #     index = hk % m
+    #     print(f"hashed {hk}, index {index}")
+    d = DictWithChaining()
+    d.insert("k1", "v1")
+    d.insert("k2", "v2")
+    d.insert("k3", "v3")
+    d.insert("k3", "v33")
+    assert d.num_items == 3
+    assert d.num_items < d.table_size
+    d.insert("k4", "v4")
+
+    # d.insert(2, "v2")
+    # d.insert("asd", "v2_asd")
+    # d.insert("adb", "v2_adb")
+    print(d)
+
+    # v = d.search("adb")
+    # assert v == "v2_adb"
