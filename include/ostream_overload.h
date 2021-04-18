5897a05641709adadb1e873d0d1d27118cbc6319
Keivan Zavari <keivan.zavari@gmail.com>
2021-04-18 14:18:04 +0000

    Add ostream for map

--- include/ostream_overload.h
+++ include/ostream_overload.h
@@ -1,13 +1,24 @@
 #pragma once
 #include <iostream>
+#include <map>
 #include <vector>
 
 template <typename T>
-std::ostream& operator<<(std::ostream& os, const std::vector<T>& container) {
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &container) {
   os << "[";
-  for (const auto& v : container) {
+  for (const auto &v : container) {
     os << v << " ";
   }
   os << "]";
+  return os;
+}
+
+template <typename T, typename S>
+std::ostream &operator<<(std::ostream &os, const std::map<T, S> &container) {
+  os << "{";
+  for (const auto &[k, v] : container) {
+    os << k << ": " << v << ", ";
+  }
+  os << "}";
   return os;
 }
