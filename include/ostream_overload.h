b57eb20103dfc6c63aff6321d58f5931833f02ef
Keivan Zavari <keivan.zavari@gmail.com>
2021-04-18 21:38:25 +0200

    Extend ostream for std set

--- include/ostream_overload.h
+++ include/ostream_overload.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include <map>
+#include <set>
 #include <unordered_map>
 #include <vector>
 
@@ -25,11 +26,20 @@ std::ostream &operator<<(std::ostream &os, const std::map<T, S> &container) {
 }
 
 template <typename T, typename S>
-std::ostream &operator<<(std::ostream &os,
-                         const std::unordered_map<T, S> &container) {
+std::ostream &operator<<(std::ostream &os, const std::unordered_map<T, S> &container) {
   os << "{";
   for (const auto &[k, v] : container) {
     os << k << ": " << v << ", ";
+  }
+  os << "}";
+  return os;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const std::set<T> &container) {
+  os << "{";
+  for (const auto &v : container) {
+    os << v << ", ";
   }
   os << "}";
   return os;
