25d4a966f1dba481bde5e74874c372b79c4b8976
Keivan Zavari <keivan.zavari@gmail.com>
2021-04-21 21:52:34 +0200

    Move bfs implementation out of class

---
+++ graphs/bfs.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <unordered_map>
+#include <vector>
+
+#include "graph_definitions.h"
+
+// BFS can also be implemented using a queue. For an example see CLRS book pp. 594
+template <typename T>
+std::unordered_map<T, T> bfs(const AdjList<T>& graph, T start) {
+  std::unordered_map<T, int> level{{start, 0}};
+  std::unordered_map<T, T> parent{{start, start}};
+  int i = 1;
+  std::vector<T> frontier{start};
+  while (!frontier.empty()) {
+    std::vector<T> next;
+    for (const auto& u : frontier) {
+      // Loop through the edges of vertex u.
+      const auto& edges = graph.at(u);
+      for (const auto& e : edges) {
+        // If vertex has not been visited.
+        if (!level.contains(e)) {
+          level[e] = i;
+          parent[e] = u;
+          next.push_back(e);
+        }
+      }
+    }
+    frontier = next;
+    ++i;
+  }
+
+  return parent;
+}
+
+template <typename T>
+std::vector<T> getShortestPath(const AdjList<T>& graph, T from, T to) {
+  auto parent_vertices = bfs(graph, from);
+  if (parent_vertices.empty() || !parent_vertices.contains(from) || !parent_vertices.contains(to)) {
+    return {};
+  }
+  std::vector<T> path;
+  T vertex = to;
+  while (vertex != from) {
+    path.push_back(vertex);
+    vertex = parent_vertices.at(vertex);
+  }
+  path.push_back(from);
+  return path;
+}
--- graphs/graph_definitions.h
+++ graphs/graph_definitions.h
@@ -45,48 +45,6 @@ class Graph {
     return true;
   }
 
-  // BFS can also be implemented using a queue. For an example see CLRS book pp. 594
-  std::unordered_map<T, T> bfs(T start) const {
-    std::unordered_map<T, int> level{{start, 0}};
-    std::unordered_map<T, T> parent{{start, start}};
-    int i = 1;
-    std::vector<T> frontier{start};
-    while (!frontier.empty()) {
-      std::vector<T> next;
-      for (const auto& u : frontier) {
-        // Loop through the edges of vertex u.
-        const auto& edges = graph_.at(u);
-        for (const auto& e : edges) {
-          // If vertex has not been visited.
-          if (!level.contains(e)) {
-            level[e] = i;
-            parent[e] = u;
-            next.push_back(e);
-          }
-        }
-      }
-      frontier = next;
-      ++i;
-    }
-
-    return parent;
-  }
-
-  std::vector<T> getShortestPath(T from, T to) const {
-    auto parent_vertices = bfs(from);
-    if (parent_vertices.empty() || !parent_vertices.contains(from) || !parent_vertices.contains(to)) {
-      return {};
-    }
-    std::vector<T> path;
-    T vertex = to;
-    while (vertex != from) {
-      path.push_back(vertex);
-      vertex = parent_vertices.at(vertex);
-    }
-    path.push_back(from);
-    return path;
-  }
-
   void print() const {
     std::cout << "--------\n";
     std::cout << "Graph:\n";
