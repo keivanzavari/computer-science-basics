f40e9174cd9e279380386f33b033503050ccdb94
Keivan Zavari <keivan.zavari@gmail.com>
2021-04-24 17:43:32 +0200

    Add more test cases for DFS

--- graphs/test/dfs_test.cc
+++ graphs/test/dfs_test.cc
@@ -7,7 +7,7 @@
 #include "dfs.h"
 // clang-format on
 
-void testOne() {
+void testClrs() {
   constexpr bool directed = true;
   Graph<char> g(directed);
   char vertices[] = {'u', 'v', 'w', 'x', 'y', 'z'};
@@ -40,10 +40,38 @@ void testDag() {
   }
   g.print();
 
-  dfs(g.get(), 'u');
+  dfs(g.get(), 'h');
+}
+
+void testBst() {
+  constexpr bool directed = true;
+  Graph<char> g(directed);
+  char vertices[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
+  std::vector<std::pair<char, char>> edges{{'d', 'b'}, {'b', 'a'}, {'b', 'c'}, {'d', 'f'},
+                                           {'f', 'e'}, {'f', 'h'}, {'h', 'g'}, {'h', 'i'}};
+
+  for (const auto e : edges) {
+    g.addEdge(e.first, e.second);
+  }
+
+  dfs(g.get(), 'd');
+}
+
+void testLine() {
+  constexpr bool directed = true;
+  Graph<char> g(directed);
+  char vertices[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
+  std::vector<std::pair<char, char>> edges{{'a', 'b'}, {'b', 'c'}, {'c', 'd'}, {'d', 'e'},
+                                           {'e', 'f'}, {'f', 'g'}, {'g', 'h'}, {'h', 'i'}};
+
+  for (const auto e : edges) {
+    g.addEdge(e.first, e.second);
+  }
+
+  dfs(g.get(), 'a');
 }
 
 int main() {
-  testDag();
+  testLine();
   return 0;
 }
