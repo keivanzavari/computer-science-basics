21cf09fdc218bc303c2b342ae4607d986f437ff8
Keivan Zavari <keivan.zavari@gmail.com>
2021-05-22 11:11:16 +0200

    Rename pointi to point_i

--- lib/geometry.py
+++ lib/geometry.py
@@ -4,21 +4,21 @@ from typing import Tuple
 from lib.point import Point
 
 
-def cross(point1: Point, point2: Point) -> Tuple[float, float, float]:
+def cross(point_1: Point, point_2: Point) -> Tuple[float, float, float]:
     # homogeneous representation of a point is (x,y,1)
-    cross_x = point1.y - point2.y
-    cross_y = point2.x - point1.x
-    cross_z = point1.x * point2.y - point1.y * point2.x
+    cross_x = point_1.y - point_2.y
+    cross_y = point_2.x - point_1.x
+    cross_z = point_1.x * point_2.y - point_1.y * point_2.x
     return cross_x, cross_y, cross_z
 
 
-def get_distance(point1: Point, point2: Point) -> float:
-    diff = point1 - point2
+def get_distance(point_1: Point, point_2: Point) -> float:
+    diff = point_1 - point_2
     return math.sqrt(diff.x * diff.x + diff.y * diff.y)
 
 
-def get_angle(point1: Point, point2: Point) -> float:
+def get_angle(point_1: Point, point_2: Point) -> float:
     # the line through two points is L=p cross p'
     # ax+by+c=0
-    a, b, _ = cross(point1, point2)
+    a, b, _ = cross(point_1, point_2)
     return math.atan2(-a, b)
