dfb0582e83bbc4d413fbeb64e2a7b7d5371a52fc
Keivan Zavari <keivan.zavari@gmail.com>
2021-05-22 20:17:02 +0200

    Add a method to normalize line coefficients

--- lib/line.py
+++ lib/line.py
@@ -18,6 +18,12 @@ class Line:
         return np.sign(self.a * point_1.x + self.b * point_1.y + self.c) == np.sign(self.a * point_2.x +
                                                                                     self.b * point_2.y + self.c)
 
+    def normalize(self) -> None:
+        sqrt_abc = np.sqrt(self.a * self.a + self.b * self.b + self.c * self.c)
+        self.a /= sqrt_abc
+        self.b /= sqrt_abc
+        self.c /= sqrt_abc
+
     def as_tuple(self) -> Tuple[float, float, float]:
         return (self.a, self.b, self.c)
 
